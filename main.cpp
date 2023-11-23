d199344979f2b0d1291d9193afece72bf297436e
Guilhem Raux <graux@c2r2s3.42lausanne.ch>
2023-11-23 17:55:11 +0100

    added a sighandle that crashes the servec on ^C

--- main.cpp
+++ main.cpp
@@ -6,19 +6,27 @@
 /*   By: graux <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 15:42:49 by graux             #+#    #+#             */
-/*   Updated: 2023/11/19 12:48:02 by graux            ###   ########.fr       */
+/*   Updated: 2023/11/23 17:48:59 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 #include <iostream>
+#include <sys/signal.h>
 
 bool	server_off = false;
+
+static void	signal_handler(int sig)
+{
+	(void) sig;
+	server_off = true;
+}
 
 int	main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
+		signal(SIGINT, signal_handler);
 		Server	serv(argv[1], argv[2]);
 		serv.lnch();
 		serv.run();
